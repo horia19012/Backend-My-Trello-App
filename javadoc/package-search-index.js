8d192401033ca6bdc7c6d29a5c55127e129c39c8
Horia Apostol <apostol_dan1901@yahoo.com>
2024-03-25 10:08:49 +0200

    javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.TaskManagement"},{"l":"com.example.TaskManagement.controller"},{"l":"com.example.TaskManagement.entity"},{"l":"com.example.TaskManagement.repository"},{"l":"com.example.TaskManagement.service.impl"}];updateSearchResults();
