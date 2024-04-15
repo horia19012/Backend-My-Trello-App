f012fab8ae6d5173620ee03fc2c906abaae4d74c
Horia Apostol <apostol_dan1901@yahoo.com>
2024-04-15 09:43:33 +0300

    started mock, tables.png

---
+++ src/test/java/com/example/TaskManagement/test/ServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.TaskManagement.test;
+
+import com.example.TaskManagement.repository.UsersRepository;
+
+public class ServiceTest {
+    @Mock
+    private Date dateMoc;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        //this.dateMoc = new Date();
+    }
+            MockitoAnnotations.initMocks(this);
+
+}
