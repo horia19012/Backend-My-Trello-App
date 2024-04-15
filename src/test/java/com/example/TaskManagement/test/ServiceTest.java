12cc5e331e0be056b5dadb9f2ff20da2729a309a
Horia Apostol <apostol_dan1901@yahoo.com>
2024-04-15 09:48:11 +0300

    table2.png

--- src/test/java/com/example/TaskManagement/test/ServiceTest.java
+++ src/test/java/com/example/TaskManagement/test/ServiceTest.java
@@ -1,16 +1,22 @@
 package com.example.TaskManagement.test;
 
+import com.example.TaskManagement.entity.User;
 import com.example.TaskManagement.repository.UsersRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Date;
 
 public class ServiceTest {
     @Mock
     private Date dateMoc;
+    private UsersRepository usersRepository;
+    private User user;
 
-    @Before
+    @BeforeAll
     public void setUp(){
         MockitoAnnotations.initMocks(this);
         //this.dateMoc = new Date();
     }
-            MockitoAnnotations.initMocks(this);
-
 }
